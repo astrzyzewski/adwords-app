29a5b54029018ab2385d2679894de146673d4c78
Andrzej Strzyżewski <astrzyzewski5@gmail.com>
2018-12-16 14:18:30 +0100

    Initial component, service and routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { RegisterCustomerComponent } from './register-customer/register-customer.component';
+
+const routes: Routes = [
+    { path: '', component: RegisterCustomerComponent }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+
 import { AppComponent } from './app.component';
+
+import { NetworkService } from './services/network.service';
+import { RegisterCustomerComponent } from './register-customer/register-customer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterCustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [NetworkService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
